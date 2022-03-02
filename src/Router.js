35d331b60f51f54d47f5ceff9283247f8fb2d4c8
KyungwonLee <kwleee91@naver.com>
2022-03-02 19:04:24 +0900

    Add:first commit. 초기 세팅 완료.

--- src/Router.js
+++ src/Router.js
@@ -1,54 +1,53 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // 송정석's 컴포넌트
-import LoginJeongseok from './pages/jeongseoksong/Login/Login';
-import MainJeongseok from './pages/jeongseoksong/Main/Main';
+// import LoginJeongseok from "./pages/jeongseoksong/Login/Login";
+// import MainJeongseok from "./pages/jeongseoksong/Main/Main";
 
 // 이경원's 컴포넌트
-import LoginKyungwonlee from './pages/kyungwonlee/Login/Login';
-import MainKyungwonlee from './pages/kyungwonlee/Main/Main';
+import LoginKyungwonlee from "./pages/kyungwonlee/Login/Login";
+import MainKyungwonlee from "./pages/kyungwonlee/Main/Main";
 
 // 이슬아's 컴포넌트
-import LoginSeula from './pages/seulaLee/Login/Login';
-import MainSeula from './pages/seulaLee/Main/Main';
+// import LoginSeula from "./pages/seulaLee/Login/Login";
+// import MainSeula from "./pages/seulaLee/Main/Main";
 
 // 전해윤's 컴포넌트
-import LoginHaeyun from './pages/haeyunjun/Login/Login';
-import MainHaeyun from './pages/haeyunjun/Main/Main';
+// import LoginHaeyun from "./pages/haeyunjun/Login/Login";
+// import MainHaeyun from "./pages/haeyunjun/Main/Main";
 
 // 최정환's 컴포넌트
-import LoginJunghwan from './pages/choijunghwan/Login/Login';
-import MainJunghwan from './pages/choijunghwan/Main/Main';
+// import LoginJunghwan from "./pages/choijunghwan/Login/Login";
+// import MainJunghwan from "./pages/choijunghwan/Main/Main";
 
 // 한서연's 컴포넌트
-import LoginSeoyeon from './pages/seoyeonHan/Login/Login';
-import MainSeoyeon from './pages/seoyeonHan/Main/Main';
-
+// import LoginSeoyeon from "./pages/seoyeonHan/Login/Login";
+// import MainSeoyeon from "./pages/seoyeonHan/Main/Main";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/login-jeongseok' element={<LoginJeongseok/>}/>
-                <Route path='/main-jeongseok' element={<MainJeongseok/>}/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/login-jeongseok" element={<LoginJeongseok />} />
+        <Route path="/main-jeongseok" element={<MainJeongseok />} /> */}
 
-                <Route path='/login-kyungwon' element={<LoginKyungwonlee/>}/>
-                <Route path='/main-kyungwon' element={<MainKyungwonlee/>}/>
+        <Route path="/login-kyungwon" element={<LoginKyungwonlee />} />
+        <Route path="/main-kyungwon" element={<MainKyungwonlee />} />
 
-                <Route path='/login-seula' element={<LoginSeula/>}/>
-                <Route path='/main-seula' element={<MainSeula/>}/>
+        {/* <Route path="/login-seula" element={<LoginSeula />} />
+        <Route path="/main-seula" element={<MainSeula />} />
 
-                <Route path='/login-haeyun' element={<LoginHaeyun/>}/>
-                <Route path='/main-haeyun' element={<MainHaeyun/>}/>
+        <Route path="/login-haeyun" element={<LoginHaeyun />} />
+        <Route path="/main-haeyun" element={<MainHaeyun />} />
 
-                <Route path='/login-junghwan' element={<LoginJunghwan/>}/>
-                <Route path='/main-junghwan' element={<MainJunghwan/>}/>
+        <Route path="/login-junghwan" element={<LoginJunghwan />} />
+        <Route path="/main-junghwan" element={<MainJunghwan />} />
 
-                <Route path='/login-seoyeon' element={<LoginSeoyeon/>}/>
-                <Route path='/main-seoyeon' element={<MainSeoyeon/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/login-seoyeon" element={<LoginSeoyeon />} />
+        <Route path="/main-seoyeon" element={<MainSeoyeon />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
---
+++ src/pages/kyungwonlee/Main/Main.js
@@ -0,0 +1,116 @@
+import React from "react";
+import "./Main.scss";
+
+function Main() {
+  return (
+    <div>
+      <header>
+        <h1 className="logo">Webucks</h1>
+        <nav>
+          <ul className="nav-links">
+            <li>COFFEE</li>
+            <li>MENU</li>
+            <li>STORE</li>
+            <li>WHAT'S NEW</li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="product">
+        <div className="product-coffee">
+          <h5>맛있는 커피</h5>
+          <i className="fa-solid fa-mug-hot"></i>
+          <p>세상에서 가장 맛있는 커피</p>
+        </div>
+
+        <ul className="coffee-list">
+          <li className="coffee">
+            <div className="img">
+              <img src="img/black-coffee.jpg" alt="블랙 커피" />
+            </div>
+            <div className="name">블랙 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/milk-coffee.jpg" alt="밀크 커피" />
+            </div>
+            <div className="name">밀크 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/mocha-coffee.jpg" alt="모카 커피" />
+            </div>
+            <div className="name">모카 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/vanila-coffee.jpg" alt="바닐라 커피" />
+            </div>
+            <div className="name">바닐라 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/caramel-coffee.jpg" alt="카라멜 커피" />
+            </div>
+            <div className="name">카라멜 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/dessert-coffee.jpg" alt="커피 그리고 디저트" />
+            </div>
+            <div className="name">커피 그리고 디저트</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/drip-coffee.jpg" alt="드립 커피" />
+            </div>
+            <div className="name">드립 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/fusioa-coffee.jpg" alt="푸지오아 커피" />
+            </div>
+            <div className="name">푸지오아 커피</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/hot-choco.jpg" alt="핫초코" />
+            </div>
+            <div className="name">핫초코</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/magic-coffee.jpg" alt="매직 커피" />
+            </div>
+            <div className="name">매직 커피</div>
+          </li>
+        </ul>
+      </section>
+
+      <section className="product">
+        <div className="product-coffee">
+          <h5>달콤한 음료</h5>
+          <i className="fa-solid fa-mug-hot"></i>
+          <p>세상에서 가장 달콤한 음료</p>
+        </div>
+
+        <ul className="coffee-list">
+          <li className="coffee">
+            <div className="img">
+              <img src="img/strawberry-shake.jpg" alt="딸기 쉐이크" />
+            </div>
+            <div className="name">딸기 쉐이크</div>
+          </li>
+          <li className="coffee">
+            <div className="img">
+              <img src="img/lemon-ade.jpg" alt="블루레몬 에이드" />
+            </div>
+            <div className="name">블루레몬 에이드</div>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Main;
