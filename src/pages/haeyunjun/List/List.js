4198c12557164d6fc5bc30090cffb0b2e1fb83a3
haeyun jun <haleyjun9@gmail.com>
2022-03-03 16:44:09 +0900

    Add: - Mission 0C -NavBar 컴포넌트로 만들기 완료 -NavBar 컴포넌트 사용하여 List페이지, Detail페이지에 적용

--- src/pages/haeyunjun/List/List.js
+++ src/pages/haeyunjun/List/List.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import './List.scss'
+import TopNav from '../TopNav.js'
 
 function List() {
     return (
         <div>
-            <nav className="navigation-bar">
-                <h1 className="webucks-logo">WeBucks</h1>
-                <ul className="menu-bar">
-                    <li className="menu-list">COFFEE</li>
-                    <li className="menu-list">MENU</li>
-                    <li className="menu-list">STORE</li>
-                    <li className="menu-list">WHAT'S NEW</li>
-                </ul>
-            </nav>
+            <TopNav/>
 
             <section className='title-bar'>
                 <h1 className="coffee-title">콜드 브루 커피 ☕ </h1>
@@ -134,3 +127,4 @@ function List() {
 }
 
 export default List
+
