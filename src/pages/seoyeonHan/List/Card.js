f17395dd580251cf6bf42686700cfffbc6f310ca
SEOYEON HAN <tjdus0824@naver.com>
2022-03-05 22:13:48 +0900

    Add: Card.scss파일 작성완료

--- src/pages/seoyeonHan/List/Card.js
+++ src/pages/seoyeonHan/List/Card.js
@@ -2,18 +2,16 @@ import React from "react";
 
 function Card({ coffee }) {
   return (
-    // <div>
-    //     <div>{coffee.name}</div>
-    //     <img src={coffee.imgUrl} alt="coffee"/>
-    // </div>
-    <li className="menuData">
-      <dl>
-        <dt className="imgDt">
-          <img src={coffee.imgUrl} alt={coffee.name} />
-        </dt>
-        <dd>{coffee.name}</dd>
-      </dl>
-    </li>
+    <>
+      <li className="menuData">
+        <dl className="coffeeMenuData">
+          <dt className="imgDt">
+            <img src={coffee.imgUrl} alt={coffee.name} />
+          </dt>
+          <dd className="coffeeNamedd">{coffee.name}</dd>
+        </dl>
+      </li>
+    </>
   );
 }
 
