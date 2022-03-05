abb4f4d5210483f60939dbf02a16c57fae9b58bb
unknown <blacksjs@naver.com>
2022-03-05 16:54:52 +0900

    폴더 및 파일명 수정

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // 송정석's 컴포넌트
 import LoginJeongseok from "./pages/jeongseoksong/Login/Login";
-import MainJeongseok from "./pages/jeongseoksong/Main/Main";
+import MainJeongseok from "./pages/jeongseoksong/List/List";
 import DetailJeongseok from "./pages/jeongseoksong/Detail/Detail";
 
 
