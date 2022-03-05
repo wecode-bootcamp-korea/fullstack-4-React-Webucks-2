36c15eedf683a3988b1a2ddb10542dda739bcc16
SEOYEON HAN <tjdus0824@naver.com>
2022-03-05 21:19:31 +0900

    public안의 data폴더 내부에 seoyeonData폴더 생성 / List.js파일 수정작업

--- src/pages/seoyeonHan/List/List.js
+++ src/pages/seoyeonHan/List/List.js
@@ -5,23 +5,58 @@ import Card from "./Card";
 
 function List() {
   const [coffeeList, setCoffeeList] = useState([]);
+  const [brewList, setBrewList] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/data/CoffeeData.json", {
-      method: "GET",
-    })
-      .then(res => res.json())
-      .then(data => {
-        setCoffeeList(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/data/CoffeeData.json", {
+  //     method: "GET",
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setCoffeeList(data);
+  //     });
+  // }, []);
 
   return (
     <div>
-      <TopNav/>
-      {coffeeList.map(coffee => (
-        <Card key={coffee.id} coffee={coffee} />
-      ))}
+      <TopNav />
+      <div className="cateBar">
+        <div className="cateName">콜드 브루 커피</div>
+        <img
+          className="decafImg"
+          src="images/logo_decaf.png"
+          alt="디카페인 로고"
+        />
+        <div className="decafInfo">
+          디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
+        </div>
+      </div>
+      <article>
+        <ul>
+          {coffeeList.map((coffee) => (
+            <Card key={coffee.id} coffee={coffee} />
+          ))}
+        </ul>
+      </article>
+
+      <div className="cateBar">
+        <div className="cateName">콜드 브루 커피</div>
+        <img
+          className="decafImg"
+          src="images/logo_decaf.png"
+          alt="디카페인 로고"
+        />
+        <div className="decafInfo">
+          디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
+        </div>
+      </div>
+      <article>
+        <ul>
+          {brewList.map((coffee) => (
+            <Card key={coffee.id} coffee={coffee} />
+          ))}
+        </ul>
+      </article>
     </div>
   );
 }
