7eceba3b2d2290b567455178ae9c157eb2789ffd
lsa <pingu944@gmail.com>
2022-03-07 15:19:02 +0900

    Modify : Login.js,Login.scss 변경

--- src/pages/seulaLee/Login/Login.js
+++ src/pages/seulaLee/Login/Login.js
@@ -1,41 +1,51 @@
 
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 
-function Login(){
-    //login btn 작동
-    const navigate = useNavigate();
-    const goUrl = () => {
-        navigate('/coffee');
-    };
+function Login() {
+    const [inputValue, setInputValue] = useState({
+        "idValue": '',
+        "pwValue": '',
+    });
+    const handleIdPwInput = (e) => { 
+        //console.log({ ...inputValue}, e.target.value,e.target.name);//중괄호 안에 inputValue 써주어야 함.inputValue 출력
+        setInputValue({...inputValue,[e.target.name]:e.target.value})//es6 문법, spread operator을 통해 객체 업데이트
+    
+    }
     return (
         <div className="gray-body">
             <div className="wrap-login">
             <article className="login-section">
                 <h2 className="hidden">로그인 박스</h2>
-                <h1 className="webucks-logo big-logo">webucks</h1>
-            
-                <section className="login-form">
+                    <h1 className="webucks-logo big-logo">webucks</h1>
+                    <InputLogin onChange={handleIdPwInput} />
+            </article>
+        </div>
+    </div>
+    );
+}
+export function InputLogin(props) { 
+    const navigate = useNavigate();
+    const goUrl = () => {
+        navigate('/coffee');
+    };
+    
+    return (
+        <section className="login-form-seula">
                 <h2 className="hidden">로그인 폼</h2>
-                    <div className="input-box">
-                        <input id="inputId" type="text" className="input-default" placeholder="전화번호, 사용자 이름 또는 이메일" />
-                    </div>
-                    <div className="input-box">
-                        <input id="inputPw" type="password" className="input-default" placeholder="비밀번호" />
-                    </div>
+
+            <input name="idValue" onChange={props.onChange} type="text" className="input-default" placeholder="전화번호, 사용자 이름 또는 이메일" />       
+                <input name="pwValue" onChange={props.onChange} type="password" className="input-default" placeholder="비밀번호" />
+                   
                     <button type='button' id="loginBtn" className="btn primary" onClick = {goUrl} >로그인</button>
 
                     <div className="form-end">
                         <span className="inline-link">비밀번호를 잊으셨나요?</span>
                     </div>
                 </section>
-                
-            </article>
-        </div>
-    </div>
     );
 }
-
 
 export default Login;
