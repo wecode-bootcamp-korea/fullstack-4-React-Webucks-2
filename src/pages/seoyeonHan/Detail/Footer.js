a3cfea9716a196b8d18fbf76faf9864c58e3bce2
SEOYEON HAN <tjdus0824@naver.com>
2022-03-09 22:26:09 +0900

    Footer.js완성 후 렌더링

--- src/pages/seoyeonHan/Detail/Footer.js
+++ src/pages/seoyeonHan/Detail/Footer.js
@@ -4,52 +4,40 @@ import "./Footer.scss";
 function Footer(){
     return(
         <div className="footerBoxAll">
-            <div className="footerBox1">
-                <ul>
-                    <li>COMPANY</li>
-                    <li>한눈에 보기</li>
-                    <li>스타벅스 사명</li>
-                    <li>스타벅스 소개</li>
-                    <li>국내 뉴스룸</li>
-                    <li>세계의 스타벅스</li>
-                    <li>글로벌 뉴스룸</li>
-                </ul>
-            </div>
-            <div className="footerBox2">
-                <ul>
-                    <li>CORPORATE SALES</li>    
-                    <li>단체 및 기업 구매 안내</li>    
-                </ul>    
-            </div>
-            <div className="footerBox3">
-                <ul>
-                    <li>PARTNERSHIP</li>    
-                    <li>신규 입점 제의</li>    
-                    <li>협력 고객사 등록 신청</li>    
-                </ul>    
-            </div>
-            <div className="footerBox4">
-                <ul>
-                    <li>ONLINE COMMUNITY</li>
-                    <li>페이스북</li>    
-                    <li>트위터</li>    
-                    <li>유튜브</li>    
-                    <li>블로그</li>    
-                    <li>인스타그램</li>    
-                </ul>    
-            </div>
-            <div className="footerBox5">
-                <ul>
-                    <li>RECRUIT</li>    
-                    <li>채용 소개</li>    
-                    <li>채용 지원하기</li>    
-                </ul>    
-            </div>
-            <div className="footerBox6">
-                <ul>
-                    <li>WEBUCKS</li>    
-                </ul>    
-            </div>
+            <ul className="footerBox">
+                <li className="footerTitle">COMPANY</li>
+                <li>한눈에 보기</li>
+                <li>스타벅스 사명</li>
+                <li>스타벅스 소개</li>
+                <li>국내 뉴스룸</li>
+                <li>세계의 스타벅스</li>
+                <li>글로벌 뉴스룸</li>
+            </ul>
+            <ul className="footerBox" >
+                <li className="footerTitle">CORPORATE SALES</li>    
+                <li>단체 및 기업 구매 안내</li>    
+            </ul>    
+            <ul className="footerBox">
+                <li className="footerTitle">PARTNERSHIP</li>    
+                <li>신규 입점 제의</li>    
+                <li>협력 고객사 등록 신청</li>    
+            </ul>    
+            <ul className="footerBox">
+                <li className="footerTitle">ONLINE COMMUNITY</li>
+                <li>페이스북</li>    
+                <li>트위터</li>    
+                <li>유튜브</li>    
+                <li>블로그</li>    
+                <li>인스타그램</li>    
+            </ul>    
+            <ul className="footerBox">
+                <li className="footerTitle">RECRUIT</li>    
+                <li>채용 소개</li>    
+                <li>채용 지원하기</li>    
+            </ul>    
+            <ul className="footerBox">
+                <li className="footerTitle">WEBUCKS</li>    
+            </ul>    
         </div>
     );
 };
